4aa159857540da66d28af15c80e77a54fcf30d02
Sakib-Anjum-313 <sakibanjum.313@gmail.com>
2023-04-27 16:04:48 +0600

    Kepler planets csv file converted into array of object

---
+++ index.js
@@ -0,0 +1,22 @@
+const { error } = require("console");
+const { parse } = require("csv-parse");
+const fs = require("fs");
+
+const results = [];
+
+fs.createReadStream("kepler_data.csv").pipe(
+    parse({
+        comment: "#",
+        columns: true,
+    })
+)
+    .on('data', (data) => {
+        results.push(data);
+    })
+    .on('error', (error) => {
+        console.log(error);
+    })
+    .on('end', () => {
+        console.log(results);
+        console.log('done');
+    });
